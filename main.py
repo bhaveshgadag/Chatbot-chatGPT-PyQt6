5daeecf2a54824600312d78a622c8737dc066272
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-09-05 14:09:48 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QTextEdit, QMainWindow, QPushButton
+
+
+class ChatWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setMinimumSize(600, 500)
+        # Chat box
+        self.chat_box = QTextEdit(self)
+        self.chat_box.setGeometry(10, 10, 400, 300)
+
+        # Input box
+        self.input_box = QTextEdit(self)
+        self.input_box.setGeometry(10, 320, 400, 30)
+
+        self.button = QPushButton("Send", self)
+        self.button.setGeometry(420, 320, 50, 30)
+
+        self.show()
+
+
+class Chatbot():
+    pass
+
+
+app = QApplication(sys.argv)
+main_window = ChatWindow()
+sys.exit(app.exec())
